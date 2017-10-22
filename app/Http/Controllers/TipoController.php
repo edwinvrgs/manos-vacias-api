108ca8885465dd0c37623bc794ae1b346ef0693f
Edwin Vargas <onix.edwin18@gmail.com>
2017-10-22 14:21:34 -0400

    Crea funcion index del TipoController

--- app/Http/Controllers/TipoController.php
+++ app/Http/Controllers/TipoController.php
@@ -2,17 +2,82 @@
 
 namespace App\Http\Controllers;
 
-class TipoController extends Controller
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
+use Illuminate\Http\Request;
+use App\Tipo;
+
+class TipoController extends Controller {
+
+    public function __construct() {
     }
 
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request) {
+        $tipos = Tipo::all();
+
+        return response()->json($tipos->toJson());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse|string
+     */
+    public function show($id) {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse|string
+     */
+    public function store(Request $request) {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id) {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse|string
+     */
+    public function destroy($id) {
+    }
+
+    /**
+     * Store Request Validation Rules
+     *
+     * @param Request $request
+     * @return array
+     */
+    private function storeRequestValidationRules(Request $request) {
+       return [
+        ];
+    }
+
+    /**
+     * Update Request validation Rules
+     *
+     * @param Request $request
+     * @return array
+     */
+    private function updateRequestValidationRules(Request $request) {
+        return [
+        ];
+    }
 }
