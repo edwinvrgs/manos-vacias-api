2f259905680b7a7e1d1ccfb224008ec06d8d3da6
Edwin Vargas <onix.edwin18@gmail.com>
2017-10-22 12:25:32 -0400

    Agrega onUpdate a las claves foraneas

--- database/migrations/2017_10_22_155646_add_foreing_keys.php
+++ database/migrations/2017_10_22_155646_add_foreing_keys.php
@@ -14,30 +14,30 @@ class AddForeingKeys extends Migration
     public function up()
     {
         Schema::table('user', function (Blueprint $table) {
-            $table->foreign('rol_id')->references('id')->on('rol')->onDelete('cascade');
+            $table->foreign('rol_id')->references('id')->on('rol')->onDelete('cascade')->onUpdate('cascade');
         });
 
         Schema::table('representante', function (Blueprint $table) {
-            $table->foreign('municipio_id')->references('id')->on('municipio')->onDelete('cascade');
-            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
+            $table->foreign('municipio_id')->references('id')->on('municipio')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade')->onUpdate('cascade');
         });
 
         Schema::table('requerimiento', function (Blueprint $table) {
-            $table->foreign('tipo_id')->references('id')->on('tipo')->onDelete('cascade');
-            $table->foreign('ninho_id')->references('id')->on('ninho')->onDelete('cascade');
+            $table->foreign('tipo_id')->references('id')->on('tipo')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('ninho_id')->references('id')->on('ninho')->onDelete('cascade')->onUpdate('cascade');
         });
 
         Schema::table('adjunto', function (Blueprint $table) {
-            $table->foreign('requerimiento_id')->references('id')->on('requerimiento')->onDelete('cascade');
+            $table->foreign('requerimiento_id')->references('id')->on('requerimiento')->onDelete('cascade')->onUpdate('cascade');
         });
 
         Schema::table('municipio', function (Blueprint $table) {
-            $table->foreign('estado_id')->references('id')->on('estado')->onDelete('cascade');
+            $table->foreign('estado_id')->references('id')->on('estado')->onDelete('cascade')->onUpdate('cascade');
         });
 
         Schema::table('ninho_cancer', function (Blueprint $table) {
-            $table->foreign('ninho_id')->references('id')->on('ninho')->onDelete('cascade');
-            $table->foreign('cancer_id')->references('id')->on('cancer')->onDelete('cascade');
+            $table->foreign('ninho_id')->references('id')->on('ninho')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('cancer_id')->references('id')->on('cancer')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
