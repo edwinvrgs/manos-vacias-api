d26145433029ad850b919b73faf84384714c401a
Jonas Gonzalez <alfredojonas94@gmail.com>
2017-10-28 10:37:40 -0400

    Cambios locales

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -92,10 +92,8 @@ $app->singleton(
 |
 */
 
-// allow origin
-header('Access-Control-Allow-Origin: *');
-// add any additional headers you need to support here
-header('Access-Control-Allow-Headers: Origin, Content-Type');
+
+
 
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
